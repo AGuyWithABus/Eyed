9ff5e1d43473a09065d30fb6f02cdccaa3d941e8
AGuyWithABus <vincentchaora@gmail.com>
2024-08-08 15:41:50 +0200

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -4,10 +4,7 @@
 apt update && apt upgrade -y
 
 # Install necessary packages
-apt install -y python clang git zip unzip wget
-
-# Install Rust (needed for some Python packages)
-pkg install rust -y
+apt install -y python clang git zip unzip wget rust
 
 # Upgrade pip to the latest version
 pip install --upgrade pip
@@ -15,15 +12,24 @@ pip install --upgrade pip
 # Install Python dependencies
 pip install firebase-admin
 
-# APKTool installation instructions (requires manual setup)
+# Install APKTool
 echo "Installing APKTool..."
 
-# Fetching the APKTool jar and wrapper script
-wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O /usr/local/bin/apktool
-wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O /usr/local/bin/apktool.jar
+# Create a bin directory in your home directory
+mkdir -p ~/bin
+
+# Download the APKTool jar and script
+wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O ~/bin/apktool
+wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O ~/bin/apktool.jar
 
 # Make the APKTool script executable
-chmod +x /usr/local/bin/apktool
+chmod +x ~/bin/apktool
 
-# Check APKTool version
+# Add the bin directory to your PATH in the current session
+export PATH=$PATH:$HOME/bin
+
+# Optionally, permanently add the bin directory to your PATH
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+# Check APKTool version to verify installation
 apktool --version
