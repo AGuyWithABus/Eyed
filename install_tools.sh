fd2a9d7baf6896bd13e218f04fc71cb497a847e8
AGuyWithABus <vincentchaora@gmail.com>
2024-08-09 16:37:51 +0200

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-# Update package lists and upgrade existing packages
-apt update && apt upgrade -y
-
-# Install necessary packages
-apt install -y python clang git zip unzip wget rust
-
-# Upgrade pip to the latest version
-pip install --upgrade pip
-
-# Install Python dependencies
-pip install firebase-admin
+# Update and install required packages
+pkg update -y
+pkg upgrade -y
 
 # Install APKTool
-echo "Installing APKTool..."
+pkg install -y apktool
 
-# Create a bin directory in your home directory
-mkdir -p ~/bin
+# Install OpenJDK for apksigner
+pkg install -y openjdk-17
 
-# Download the APKTool jar and script
-wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O ~/bin/apktool
-wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O ~/bin/apktool.jar
+# Install adb
+pkg install -y android-tools
 
-# Make the APKTool script executable
-chmod +x ~/bin/apktool
-
-# Add the bin directory to your PATH in the current session
-export PATH=$PATH:$HOME/bin
-
-# Optionally, permanently add the bin directory to your PATH
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-
-# Check APKTool version to verify installation
-apktool --version
+echo "Tools installed successfully."
